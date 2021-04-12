d1e4e238ab0d46c851d4872d3005a769f96f6721
MattCornicDev <glgharib@gmail.com>
2021-04-12 15:49:02 +0200

    init

---
+++ covid19.js
@@ -0,0 +1,16 @@
+
+    var LiveDataByDepartement = [
+        {
+            "code": "DEP-69",
+            "nom": "Rhône",
+            "date": "2020-04-27",
+            "hospitalises": 1151,
+            "reanimation": 175,
+            "deces": 481,
+            "gueris": 1684,
+            "source": {
+                "nom": "Santé publique France Data"
+            },
+            "sourceType": "sante-publique-france-data"
+        }
+    ]
